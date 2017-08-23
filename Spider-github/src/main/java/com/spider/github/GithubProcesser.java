9890da02ff642b33772c212ac2df42be39a6d918
kekewang <wangke59@hotmail.com>
2017-08-23 16:12:27 +0800

    Spider: Add

--- Spider-github/src/main/java/com/spider/github/GithubProcesser.java
+++ Spider-github/src/main/java/com/spider/github/GithubProcesser.java
@@ -1,7 +1,18 @@
 package com.spider.github;
 
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.processor.PageProcessor;
+
 /**
  * Created by jeanqinhu on 2017/8/22.
  */
-public class GithubProcesser {
+public class GithubProcesser implements PageProcessor {
+    public void process(Page page) {
+
+    }
+
+    public Site getSite() {
+        return null;
+    }
 }
