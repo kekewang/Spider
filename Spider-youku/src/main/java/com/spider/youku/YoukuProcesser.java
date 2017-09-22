6819737e72b078e72317ca9a3f0222e963f14f9e
wangke <wangke@hotmail.com>
2017-09-22 15:54:53 +0800

    Spider: Add youku spider module.

---
+++ Spider-youku/src/main/java/com/spider/youku/YoukuProcesser.java
@@ -0,0 +1,18 @@
+package com.spider.youku;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+/**
+ * Created by wangke on 2017/9/22.
+ */
+public class YoukuProcesser implements PageProcessor {
+    public void process(Page page) {
+
+    }
+
+    public Site getSite() {
+        return null;
+    }
+}
