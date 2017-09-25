20cb61e036e96232ebfa358bca576476b93da846
wangke <wangke@hotmail.com>
2017-09-25 18:06:15 +0800

    Spider:  Fetch the home page of youku.

--- Spider-youku/src/main/java/com/spider/youku/YoukuProcesser.java
+++ Spider-youku/src/main/java/com/spider/youku/YoukuProcesser.java
@@ -2,17 +2,26 @@ package com.spider.youku;
 
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
 
 /**
  * Created by wangke on 2017/9/22.
  */
 public class YoukuProcesser implements PageProcessor {
-    public void process(Page page) {
 
+    private Site site = Site.me().setRetryTimes(5).setSleepTime(1000).setTimeOut(10000);
+
+    public void process(Page page) {
+        page.putField("href", page.getHtml().xpath("//div[@class='p-thumb']/a/@href").toString());
+        page.putField("title", page.getHtml().xpath("//div[@class='p-thumb']/a/@title").toString());
     }
 
     public Site getSite() {
-        return null;
+        return site;
+    }
+
+    public static void main(String[] args){
+        Spider.create(new YoukuProcesser()).addUrl("http://www.youku.com/").thread(5).run();
     }
 }
