031b7c71ba8aa88c3d1d029f85cbdda6c23b3f4d
wangke <wangke@hotmail.com>
2017-09-28 16:37:20 +0800

    Spider:  Fetch the video properties, vid, title, class, url in youku.

---
+++ Spider-youku/src/main/java/com/spider/utils/StringUtils.java
@@ -0,0 +1,19 @@
+package com.spider.utils;
+
+/**
+ * Created by wangke on 2017/9/28.
+ */
+public class StringUtils {
+    /**
+     * 是否为空
+     * @param str 字符串
+     * @return true 空 false 非空
+     */
+    public static Boolean isEmpty(String str) {
+        if(str == null || str.equals("")) {
+            return true;
+        }
+
+        return false;
+    }
+}
--- Spider-youku/src/main/java/com/spider/youku/YoukuProcesser.java
+++ Spider-youku/src/main/java/com/spider/youku/YoukuProcesser.java
@@ -1,5 +1,6 @@
 package com.spider.youku;
 
+import com.spider.utils.StringUtils;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
@@ -12,9 +13,22 @@ public class YoukuProcesser implements PageProcessor {
 
     private Site site = Site.me().setRetryTimes(5).setSleepTime(1000).setTimeOut(10000);
 
+    public static int pageCount = 0;
+
     public void process(Page page) {
-        page.putField("href", page.getHtml().xpath("//div[@class='p-thumb']/a/@href").toString());
-        page.putField("title", page.getHtml().xpath("//div[@class='p-thumb']/a/@title").toString());
+        //http://v.youku.com/v_show/id_XMzA0OTI4NjcxMg==.html?spm=a2hww.20027244.m_250036.5~5!2~5~5!3~5~5~A
+        page.addTargetRequests(page.getHtml().links().regex("//v.youku.com/v_show/id_\\w+.*").all());
+        page.putField("url", page.getUrl().toString());
+        page.putField("title", page.getHtml().xpath("//div[@class='base_info']/h1[@class='title']/allText()").toString());
+        page.putField("class",page.getHtml().xpath("//h1[@class='title']/a/text()").toString());
+        page.putField("classUrl", page.getHtml().xpath("//h1[@class='title']/a/@href"));
+        page.putField("vid",page.getUrl().regex("v.youku.com/v_show/id_([\\w+]*)==").toString());
+
+        if (page.getResultItems().get("vid") == null){
+            //skip this page
+            page.setSkip(true);
+        }
+        System.out.println("=========================Page No." + pageCount++ + "=========================");
     }
 
     public Site getSite() {
@@ -22,6 +36,6 @@ public class YoukuProcesser implements PageProcessor {
     }
 
     public static void main(String[] args){
-        Spider.create(new YoukuProcesser()).addUrl("http://www.youku.com/").thread(5).run();
+        Spider.create(new YoukuProcesser()).addUrl("http://www.youku.com/").thread(20).run();
     }
 }
